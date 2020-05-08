ef94040b6bc458f282a67a5c67f7b6267de9b91f
Andy Little <kwikius@yahoo.com>
2020-05-08 13:56:31 +0100

    calculate storage var offsets at compiletime

--- ArduIMU/storage.cpp
+++ ArduIMU/storage.cpp
@@ -28,7 +28,7 @@ namespace {
       Vect3F,  // [ MAG_OFST ] 
       Vect3F,  // [ MAG_GAIN ]
       Bool,    // [ MAG_CALIBRATED ]
-      Uint8 ,  // [ RUNMODE ]
+      Uint8 ,  // [ RUN_MODE ]
       Vect3F,  // [ ACC_OFST ]
       Vect3F,  // [ ACC_GAIN ]
       Bool,    // [ ACC_CALIBRATED ]
@@ -40,18 +40,44 @@ namespace {
    // gives a bit of space at start of eeprom for 
    // other bits an pieces later
    constexpr uint16_t storageStartIdx = 0x10;
-}
+
+   template <storageID In>
+   constexpr uint16_t llGetStorageAddress();
+
+   template <>
+   constexpr uint16_t llGetStorageAddress<MAG_OFST>() { return storageStartIdx;}
+
+   template <storageID In>
+   constexpr uint16_t llGetStorageAddress()
+   {
+       return  llGetStorageAddress<static_cast<storageID>(static_cast<uint16_t>(In)-1) >()
+            + storageSizeFromType[
+                 static_cast<uint16_t>(storageTypes[ static_cast<uint16_t>(In)-1 ])
+               ] ;
+   }
+ 
+   uint16_t constexpr storageIndexes[] =
+   {
+       llGetStorageAddress<MAG_OFST>(),
+       llGetStorageAddress<MAG_GAIN>(),
+       llGetStorageAddress<MAG_CALIBRATED>(),
+       llGetStorageAddress<RUN_MODE>(),
+       llGetStorageAddress<ACC_OFST>(),
+       llGetStorageAddress<ACC_GAIN>(),
+       llGetStorageAddress<ACC_CALIBRATED>(),
+       llGetStorageAddress<GYR_OFST>(),
+       llGetStorageAddress<GYR_GAIN>(),
+       llGetStorageAddress<GYR_CALIBRATED>()
+   };
+
+   static_assert( sizeof(storageIndexes) / sizeof(uint16_t) == static_cast<int>(STORAGE_ID_END),"" );
+
+} // namespace
 
 uint16_t getStorageAddress(storageID in)
 {
-   return (static_cast<uint16_t>(in) == 0) 
-      ? storageStartIdx
-      : ( getStorageAddress( static_cast<storageID>(static_cast<uint16_t>(in)-1) )
-         + storageSizeFromType[
-              static_cast<uint16_t>(storageTypes[ static_cast<uint16_t>(in)-1 ])
-            ] );
+   return storageIndexes[static_cast<uint16_t>(in)];
 }
-
 
 bool readFromStorage(uint16_t storageAddress, uint8_t* runtimeAddress, uint16_t numBytes)
 {
@@ -71,9 +97,3 @@ bool writeToStorage(const uint8_t* runtimeAddress, uint16_t storageAddress, uint
     }
     return true;
 }
-
-
-
-
-
-
