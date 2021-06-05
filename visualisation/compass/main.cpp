48d378f490c4fc696096a001e012930f6337b68b
Andy Little <kwikius@yahoo.com>
2021-06-05 23:46:45 +0100

    revamp magonly visualisation

--- visualisation/compass/main.cpp
+++ visualisation/compass/main.cpp
@@ -15,20 +15,26 @@ https://cs.lmu.edu/~ray/notes/openglexamples/
 #include <quan/out/angle.hpp>
 #include <quan/fixed_quantity/operations/atan2.hpp>
 #include <quan/three_d/rotation.hpp>
+#include <quan/three_d/rotation_from.hpp>
+#include <quan/three_d/quatToMatrixOpenGL.hpp>
+
+#include <quan/three_d/vect_make_row_matrix_view.hpp>
+#include <quan/three_d/vect_make_diagonal_matrix_view.hpp>
+#include <quan/basic_matrix/make_vect3.hpp>
 
 int parse_sp(quan::serial_port& sp, quan::three_d::vect<double> & out);
 
 void reshape(GLint w, GLint h) {
-  glViewport(0, 0, w, h);
-  GLfloat aspect = (GLfloat)w / (GLfloat)h;
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
+   glViewport(0, 0, w, h);
+   GLfloat aspect = (GLfloat)w / (GLfloat)h;
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
 
-  if (w <= h) {
-    glOrtho(-1,1, -1/aspect, 1/aspect, -1.0, 1.0);
-  } else {
-    glOrtho(-aspect, aspect, -1.0, 1.0, -1.0, 1.0);
-  }
+   if (w <= h) {
+      glOrtho(-1,1, -1/aspect, 1/aspect, -1.0, 1.0);
+   }  else {
+      glOrtho(-aspect, aspect, -1.0, 1.0, -1.0, 1.0);
+   }
 }
 
 namespace {
@@ -49,68 +55,144 @@ namespace {
    }
    GLfloat incr = 10;
 
+   quan::three_d::vect<double> mag_vector= {1,0,0};
 
-  // axis and angle to rotate compass needle from pointing out along x axis
-  quan::three_d::vect<double> mag_rotation_axis {1,0,0};
-  quan::angle::deg mag_rotation_angle{10};
-  quan::three_d::vect<GLfloat> display_rotation{0,0,0};
+   quan::three_d::vect<int> constexpr mag_vector_sign = {
+      1,
+      1,
+      1
+   };
+   // n.b representing angles in degrees
+   quan::three_d::vect<GLfloat> display_rotation{110,0,200};
  
-   // drawing
-
    typedef quan::three_d::vect<GLfloat> quan_vectf;
 
    void quanGLVertex(quan_vectf const & v)
    {
-        glVertex3f(v.x,v.y,v.z);
+      glVertex3f(v.x,v.y,v.z);
    }
 
    void quanGLColor(quan_vectf const & v)
    {
-        glColor3f(v.x,v.y,v.z);
+      glColor3f(v.x,v.y,v.z);
    }
 
-   quan_vectf needle_point{ 0.3f,0.f,0.f};
-   GLfloat constexpr base_x = -0.1f;
-   GLfloat constexpr base_size_y = 0.05f;
-   GLfloat constexpr base_size_z = 0.05f;
-   quan_vectf needle_arrow_base[] = {
-      {base_x, base_size_y/2.f,-base_size_z/2.f},
-      {base_x, base_size_y/2.f,base_size_z/2.f},
-      {base_x, -base_size_y/2.f,base_size_z/2.f},
-      {base_x, -base_size_y/2.f,-base_size_z/2.f}
-   };
-
-   auto constexpr num_faces = sizeof(needle_arrow_base)/ sizeof(needle_arrow_base[0]);
-
-   quan_vectf needle_colors[] = {
+   quan_vectf arrow_colours[] = {
       {0.0f, 0.7f, 0.1f}, // green
       {0.9f, 1.0f, 0.0f}, // yellow
       {0.2f, 0.2f, 1.0f}, // blue
       {0.7f, 0.0f, 0.1f}  // red
    };
+   unsigned constexpr green = 0;
+   unsigned constexpr yellow = 1;
+   unsigned constexpr blue = 2;
+   unsigned constexpr red = 3;
 
-   auto const num_colors = sizeof(needle_colors)/ sizeof(needle_colors[0]);
+   auto constexpr num_colors = sizeof(arrow_colours)/ sizeof(arrow_colours[0]);
 
-   static_assert(num_faces == num_colors,"");
+   void draw_line( quan_vectf const & from, quan_vectf const & to)
+   {
+      glBegin(GL_LINES);
+         quanGLVertex(from);
+         quanGLVertex(to);
+      glEnd();
+   }
+
+   void draw_arrow( quan_vectf const & v, GLfloat length, quan_vectf fg, quan_vectf bg)
+   {
+       quan_vectf constexpr base = {1,0,0};
+       auto q = rotation_from(base,v);
+       quan::basic_matrix<4,4,float> m = {
+         1,0,0,0,
+         0,1,0,0,
+         0,0,1,0,
+         0,0,0,1
+       };
+
+      quatToMatrixOpenGL(q,m);
+      glPushMatrix();
+      glMultMatrixf(m.get_array());
+
+      quan_vectf  point={length,0,0};
+      GLfloat const brim = point.x - 0.15;
+      GLfloat constexpr brim_radius = 0.04f;
+      int constexpr n_segs = 10;
+
+      glBegin(GL_TRIANGLES);
+     
+      for (int i = 0; i < n_segs; ++i){
+
+         quan::angle::deg const a = 360_deg * i / n_segs;
+         quan::angle::deg const b = 360_deg * ((i +1) % n_segs) / n_segs;
+         quan_vectf const aa = {brim, sin(a) * brim_radius,cos(a) * brim_radius};
+         quan_vectf const bb = {brim, sin(b) * brim_radius,cos(b) * brim_radius};
+
+         quanGLColor(fg);
+         quanGLVertex(aa);
+         quanGLVertex(point);
+         quanGLVertex(bb);
+
+         quanGLColor(bg);
+         quanGLVertex(bb);
+         quanGLVertex(quan_vectf{brim,0,0});
+         quanGLVertex(aa);
+      }
+      glEnd();
+       
+      quanGLColor(fg);
+      draw_line({0,0,0},{brim,0,0});
+      glPopMatrix();
+   }
 
    void draw_compass()
    {
-      glBegin(GL_TRIANGLES);
-         for ( auto i = 0U; i < num_faces;++i){
-            quanGLColor(needle_colors[i]);
-            quanGLVertex(needle_arrow_base[i]);
-            quanGLVertex(needle_point);
-            quanGLVertex(needle_arrow_base[(i+1) % num_faces]);
-         }
-      glEnd();
-
-      glBegin(GL_QUADS);
-         glColor3f(0.5f,0.2f,0.2f);
-         for ( auto i = 0U; i < 4;++i){
-            quanGLVertex(needle_arrow_base[i]);
-         }
-      glEnd();
+     if ( magnitude( mag_vector) > 0.01){
+      
+        quan_vectf mag_vect1 = make_vect3(
+          make_row_matrix_view(unit_vector(mag_vector)) * 
+             make_diagonal_matrix_view(mag_vector_sign)
+        );
+        draw_arrow(mag_vect1, 0.5f, arrow_colours[yellow], (arrow_colours[red] + arrow_colours[yellow] )/2 );
+     }
    }
+
+   void draw_grid(quan_vectf const & min_corner, GLfloat spacing, int nx, int ny)
+   {
+      for ( int x = 0; x < nx ;++x){
+         draw_line(
+            min_corner + quan_vectf{spacing * x,0,0}, 
+            min_corner + quan_vectf{spacing * x, spacing * ny,0}
+         );
+      }
+
+      for ( int y = 0; y < ny ;++y){
+         draw_line(
+            min_corner + quan_vectf{0,spacing * y,0}, 
+            min_corner + quan_vectf{spacing * nx, spacing * y,0}
+         );
+      }
+   }
+
+   void draw_grid()
+   {
+      GLfloat const grid_size = 0.9;
+      int const n = 5;
+      quan_vectf const min_corner{0,0,0};
+
+      quanGLColor({0.5f, 0.5f, 0.5f}) ;
+      draw_grid(min_corner,grid_size / n, n+1, n+1);
+      
+   }
+
+   void draw_axes()
+   {
+      draw_arrow({1,0,0}, 1.f, arrow_colours[red], (arrow_colours[blue] + arrow_colours[green] )/2 );
+
+      draw_arrow({0,1,0},1.f,arrow_colours[green],  (arrow_colours[blue] + arrow_colours[red] )/2 );
+
+      draw_arrow({0,0,1},1.f,arrow_colours[blue],  (arrow_colours[red] + arrow_colours[green] )/2 );
+   }
+
 }
 
 void display() {
@@ -121,37 +203,17 @@ void display() {
 
    glLoadIdentity();
 
-   glRotatef(180.f,0,0,1);
-
+   // hence euler angles
    glRotatef(display_rotation.x,1,0,0);
    glRotatef(display_rotation.y,0,1,0);
    glRotatef(display_rotation.z,0,0,1);
 
-   glBegin(GL_LINES);
-      // rgb --> xyz
-      glColor3f(0.7, 0.0, 0.1);    // red  x
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(1.0, 0.0, 0.0);
+   draw_grid();
 
-      glColor3f(0.0, 0.7, 0.1);   // green y
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(0.0, 1.0, 0.0);
-
-      glColor3f(0.2, 0.2, 1.0);    //blue z
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(0.0, 0.0, 1.0);
-
-   glEnd();
-
-   glRotatef(
-      mag_rotation_angle.numeric_value(),
-      mag_rotation_axis.x,
-      mag_rotation_axis.y,
-      mag_rotation_axis.z
-   );
+   draw_axes();
 
    draw_compass();
-  
+
    glFlush();
    glutSwapBuffers();
 }
@@ -159,75 +221,18 @@ void display() {
 namespace {
    // for read mag data from port
    quan::serial_port * serial_port = nullptr;
-
-   template <typename T>
-   inline 
-   quan::angle::rad get_rotation_angle( 
-      quan::three_d::vect<T> const & vecta, 
-      quan::three_d::vect<T> const & vectb
-   )
-   {
-      return std::acos(dot_product(unit_vector(vecta),unit_vector(vectb)));
-   }
-
-/*
-   before entry
-     // magnitude(a) > eps && magnitude(b) > eps &&
-    // magnitude( a- b) > x && magnitude ( a+ b) > x &&
-    // check that unit_vector(a) != unit_vector(b) 
-    // check that unit_vector(a) + unit_vector(b)  != [0,0,0];
-*/
-   template <typename T>
-   quan::three_d::vect<typename quan::meta::binary_op<T,quan::meta::divides,T>::type> 
-   get_rotation_axis(quan::three_d::vect<T> const & vecta, quan::three_d::vect<T> const & vectb)
-   { 
-      return unit_vector(cross_product(vecta,vectb));
-   }
-
-   double constexpr magnitude_epsilon = 0.001;
-
-   int constexpr x_sign = 1;
-   int constexpr y_sign = 1;
-   int constexpr z_sign = -1;
-
 } // namespace
 
 void onIdle()
 {
   // read input mag vector to local if new data available
-  quan::three_d::vect<double> ll_mag_vector;
-  
-  if ( parse_sp(*serial_port, ll_mag_vector) == 1 ){
-      quan::three_d::vect<double> const temp
-      { ll_mag_vector.x * x_sign,
-        ll_mag_vector.y * y_sign,
-        ll_mag_vector.z * z_sign};
-      
-      auto v1 = unit_vector(temp);
-      // sanity check against zero length
-      if ( magnitude(v1) > magnitude_epsilon ){
-         quan::three_d::vect<double> constexpr base{1,0,0};
-         // check for opposite direction to base
-         if ( magnitude(base + v1) < magnitude_epsilon ){
-            //OK so rotate 180 around y or z
-            mag_rotation_axis = quan::three_d::vect<double>{0,1,0};
-            mag_rotation_angle = quan::angle::deg{180};
-         }else{
-              //check for parallel to base
-              if ( magnitude( base - v1) < magnitude_epsilon ){
-                   // identity
-                   mag_rotation_axis = base;
-                   mag_rotation_angle = quan::angle::deg{0};
-              }else{
-                  mag_rotation_axis = get_rotation_axis(base,v1);
-                  mag_rotation_angle = get_rotation_angle(base,v1);
-              }
-         }
-         glutPostRedisplay();
-      }
+
+   if ( parse_sp(*serial_port, mag_vector) == 1 ){
+      glutPostRedisplay();
    }
 }
 
+#if 0
 namespace {
 
    // for testing
@@ -252,6 +257,7 @@ void onTimer(int value)
    glutPostRedisplay();
    glutTimerFunc(100U,onTimer,1);
 }
+#endif
 
 void onKeyboard(unsigned char key,int x, int y)
 {
@@ -290,7 +296,7 @@ void onKeyboard(unsigned char key,int x, int y)
 int main(int argc, char** argv) {
 
    serial_port= new quan::serial_port("/dev/ttyUSB0");
-   serial_port->init(B38400);
+   serial_port->init(B115200);
    if (!serial_port->good()){
       std::cout << "serial port open failed\n";
       delete serial_port;
